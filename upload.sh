1bec9935e5ed58a41749d3fa2af4b022aaf41f60
Viktor Szépe <viktor@szepe.net>
2023-10-22 14:05:58 +0200

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -4,6 +4,9 @@
 
 # Check to see if it has changed
 git status --short rfcs/* refs.json | grep -s "M" || exit 0
+if [ "$(git diff --raw | cut -f2)" = "rfcs/rfc-index.txt" ] && [ "$(git diff -U0 | grep '@@')" = "@@ -8 +8 @@" ]; then
+    exit 0
+fi
 
 # setup
 git config user.email mnot@mnot.net
@@ -11,14 +14,8 @@ git config user.name mnot-bot
 git remote set-url --push origin https://mnot:$GITHUB_TOKEN@github.com/mnot/rfc-refs
 git checkout -B main origin/master
 
-# Add the changes
+# Push the changes
 git add rfcs/*
 git add refs.json
-
-if [ "$(git diff --raw | cut -f2)" = "rfcs/rfc-index.txt" ] && [ "$(git diff -U0 | grep '@@')" = "@@ -8 +8 @@" ]; then
-    exit 0
-fi
-
-# Push the changes
 git commit -m "update refs"
 git push origin main
