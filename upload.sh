d68794f949135dd1a6d34b4fa50703e904096c3b
Viktor Szépe <viktor@szepe.net>
2023-12-17 11:21:07 +0000

    simple push

--- upload.sh
+++ upload.sh
@@ -3,19 +3,16 @@
 # This script is for CI updates
 
 # Check to see if it has changed
-git status --short rfcs/* refs.json | grep -s "M" || exit 0
+git diff --quiet -- rfcs/ refs.json && exit 0
 if [ "$(git diff --raw | cut -f2)" = "rfcs/rfc-index.txt" ] && [ "$(git diff -U0 | grep '@@')" = "@@ -8 +8 @@" ]; then
     exit 0
 fi
 
 # setup
-git config user.email mnot@mnot.net
-git config user.name mnot-bot
-git remote set-url --push origin https://mnot:$GITHUB_TOKEN@github.com/mnot/rfc-refs
-git checkout -B main origin/master
+git config --local user.email mnot@mnot.net
+git config --local user.name mnot-bot
 
 # Push the changes
-git add rfcs/*
-git add refs.json
+git add -- rfcs/ refs.json
 git commit -m "update refs"
-git push origin main
+git push
